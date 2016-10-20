99fe09d5cac8715dc912373744db87e9b49bd6d3
Maximilian De Oliveira-Spiker <s3476696@student.rmit.edu.au>
2016-10-20 17:01:51 +1100

    API code

---
+++ API/ProsocWebsite/src/com/prosoc/entity/User.java
@@ -0,0 +1,86 @@
+package com.prosoc.entity;
+
+import javax.jdo.annotations.IdGeneratorStrategy; 
+import javax.jdo.annotations.IdentityType; 
+import javax.jdo.annotations.PersistenceCapable; 
+import javax.jdo.annotations.Persistent; 
+import javax.jdo.annotations.PrimaryKey;   
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION) 
+public class User {
+	@PrimaryKey 
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)   
+	Long id;
+	@Persistent
+	String username;
+	@Persistent
+	String name;
+	@Persistent
+	Long phone;
+	@Persistent
+	String email;
+	@Persistent
+	String password;
+	@Persistent
+	Long contactId;
+	@Persistent
+	Long dob;
+	@Persistent
+	String gender;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getPhone() {
+		return phone;
+	}
+	public void setPhone(Long phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Long getContactId() {
+		return contactId;
+	}
+	public void setContactId(Long contactId) {
+		this.contactId = contactId;
+	}
+	public Long getDob() {
+		return dob;
+	}
+	public void setDob(Long dob) {
+		this.dob = dob;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+}
